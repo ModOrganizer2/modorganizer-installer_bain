ec2cdd34d2adf93ee5b01c83064c0e4ed4a01d0e
Tannin <sherb@gmx.net>
2016-06-04 20:35:28 +0200

    translation

---
+++ src/installer_bain_en.ts
@@ -0,0 +1,78 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>BainComplexInstallerDialog</name>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="14"/>
+        <source>BAIN Package Installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="22"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="38"/>
+        <source>This looks like a Package prepared for Installation through BAIN. You can select options from the list below. If there is a package.txt file, it should contain detailed information about the options.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="48"/>
+        <source>Components of this package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="51"/>
+        <source>Components of this package.
+If there is a component called &quot;00 Core&quot; it is usually required. Options are ordered by priority as set up by the author.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="61"/>
+        <location filename="baincomplexinstallerdialog.ui" line="64"/>
+        <source>The package.txt is often part of BAIN packages and contains details about the options available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="87"/>
+        <location filename="baincomplexinstallerdialog.ui" line="90"/>
+        <source>Opens a Dialog that allows custom modifications.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="93"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="100"/>
+        <source>Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="baincomplexinstallerdialog.ui" line="107"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstallerBAIN</name>
+    <message>
+        <location filename="installerbain.cpp" line="56"/>
+        <source>Installer for BAIN archives (originally targeting Wrye Bash)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerbain.cpp" line="135"/>
+        <source>May be BAIN installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="installerbain.cpp" line="136"/>
+        <source>This installer looks like it may contain a BAIN installer but I&apos;m not sure. Install as BAIN installer?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
